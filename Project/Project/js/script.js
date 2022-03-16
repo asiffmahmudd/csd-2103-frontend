d707b338cb4bb3c13a39ca9a8166b790bccbd333
Asif Mahmud <asifmahmud3472@gmail.com>
2022-03-15 23:41:02 -0400

    project work in progress

--- Project/Project/js/script.js
+++ Project/Project/js/script.js
@@ -78,4 +78,24 @@ $(document).ready(function(){
 		//preventing default behavior
 		evt.preventDefault();
 	});
+
+
+    //slide for images
+    $("#image_list a").each(function() {        
+        // getting the attributes for the image
+        var url = $(this).attr("href");
+		var title = $(this).attr("title");
+
+        // preload the images
+		var img = new Image();
+		img.src = url;
+
+        // event handler for the click and changing the picture in the gallery
+        $(this).click(function(evt){
+            $("#gallery img").attr('src', url);
+            $("#caption").text(title);
+
+            evt.preventDefault();
+        })
+	});
 });
